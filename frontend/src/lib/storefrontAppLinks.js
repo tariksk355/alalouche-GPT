dbbf1f2ba5ca3535fa8558e4f64cf099aa9295fa
tariksk355 <tarikyegen35@gmail.com>
2026-05-24 20:58:17 +0200

    IOS and Android app links added to envs

--- frontend/src/lib/storefrontAppLinks.js
+++ frontend/src/lib/storefrontAppLinks.js
@@ -1,2 +1,2 @@
-export const IOS_APP_STORE_URL = 'REPLACE_WITH_APP_STORE_URL';
-export const ANDROID_PLAY_STORE_URL = 'REPLACE_WITH_GOOGLE_PLAY_URL';
+export const IOS_APP_STORE_URL = 'https://apps.apple.com/ch/app/%C3%A0-la-louche/id6761898762?l=fr-FR';
+export const ANDROID_PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.alalouche.mobile';
